6582734a4169d23cd508dd8a5c20fb7cc57ee8d5
Administrator <markovici089@gmail.com>
2024-01-19 23:27:20 +0100

    Valjda radi

---
+++ Blagajna.DB/SqlConnectionFactory.cs
@@ -0,0 +1,50 @@
+﻿using MySqlConnector;
+using System;
+using System.Configuration;
+
+namespace Blagajna.DB
+{
+    public class SqlConnectionFactory
+    {
+        public static string SqlConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["Baza"].ConnectionString;
+            }
+        }
+
+
+        public MySqlConnection GetNewConnection()
+        {
+            try
+            {
+                var connection = new MySqlConnection(SqlConnectionString);
+                connection.Open();
+
+                return connection;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void CloseConnection(MySqlConnection connection)
+        {
+            try
+            {
+                if (connection != null && connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
